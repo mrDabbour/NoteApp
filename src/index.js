8619cc5fc5a443513c08e7e633b091ce1eef649d
Mohammed Dabour <jmomj2001@gmail.com>
2024-02-09 22:30:58 +1300

    Update SRC

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />,document.getElementById('root'));
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// // ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+// const Text = {
+//   fontFamily: "Arial, sans-serif",
+//   color: "blue",
+// };
+
+// root.render(
+//   <div>
+//     <h1>Hello from ReactJs</h1>
+//     <h1 style={Text}>
+//       My name is Mohammed Dabboor
+//     </h1>
+//   </div>
+// );
